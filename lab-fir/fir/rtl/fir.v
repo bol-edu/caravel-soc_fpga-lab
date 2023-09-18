68377551d6f5ea658872ba0e905c769b930b79b9
ZheChen-Bill <88698677+ZheChen-Bill@users.noreply.github.com>
2023-09-18 18:21:40 +0800

    Add files via upload

---
+++ lab-fir/fir/rtl/fir.v
@@ -0,0 +1,37 @@
+module fir 
+#(  parameter pADDR_WIDTH = 12,
+    parameter pDATA_WIDTH = 32,
+    parameter Tape_Num    = 11
+)
+(
+    output  wire                     awready,
+    output  wire                     wready,
+    input   wire                     awvalid,
+    input   wire [(pADDR_WIDTH-1):0] awaddr,
+    input   wire                     wvalid,
+    input   wire [(pDATA_WIDTH-1):0] wdata,
+    output  wire                     arready,
+    input   wire                     rready,
+    input   wire                     arvalid,
+    input   wire [(pADDR_WIDTH-1):0] araddr,
+    output  wire                     rvalid,
+    output  wire [(pDATA_WIDTH-1):0] rdata,    
+    output  wire                     ss_tvalid, 
+    input   wire [(pDATA_WIDTH-1):0] ss_tdata, 
+    input   wire                     ss_tlast, 
+    input   wire                     ss_tready, 
+    input   wire                     sm_tready, 
+    output  wire                     sm_tvalid, 
+    output  wire [(pDATA_WIDTH-1):0] sm_tdata, 
+    output  wire                     sm_tlast, 
+    input   wire                     axis_clk,
+    input   wire                     axis_rst_n,
+    input   wire                     ap_start,
+    output  wire                     ap_done
+);
+begin
+
+    // write your code here!
+
+
+end
