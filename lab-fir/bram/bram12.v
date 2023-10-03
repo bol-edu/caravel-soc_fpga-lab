adf76abf97cc2a6f4d2be45690836e303f2d503c
Bridge of Life Education Lab <98332019+boledulab@users.noreply.github.com>
2023-10-04 06:23:59 +0800

    Update bram12.v - fix byte-write issue

--- lab-fir/bram/bram12.v
+++ lab-fir/bram/bram12.v
@@ -30,11 +30,10 @@ module bram12
     reg [31:0] Temp_D;
     always @(posedge CLK) begin
         if(EN) begin
-            Temp_D = RAM[A>>2];
-            if(WE[0]) RAM[A>>2] <= {Temp_D[31:24], Temp_D[23:16], Temp_D[15:8], Di[7:0]};
-            if(WE[1]) RAM[A>>2] <= {Temp_D[31:24], Temp_D[23:16], Di[15:8], Temp_D[7:0]};
-            if(WE[2]) RAM[A>>2] <= {Temp_D[31:24], Di[23:16], Temp_D[15:8], Temp_D[7:0]};
-            if(WE[3]) RAM[A>>2] <= {Di[31:24], Temp_D[23:16], Temp_D[15:8], Temp_D[7:0]};
+	    if(WE[0]) RAM[A>>2][7:0] <= Di[7:0];
+            if(WE[1]) RAM[A>>2][15:8] <= Di[15:8];
+            if(WE[2]) RAM[A>>2][23:16] <= Di[23:16];
+            if(WE[3]) RAM[A>>2][31:24] <= Di[31:24];
         end
     end
 
