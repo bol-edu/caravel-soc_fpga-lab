9482b22ea404c7a60766ee7772691b25e6a92494
ZheChen-Bill <bb890825@gmail.com>
2023-10-17 22:31:24 +0800

    slash for X[n]

--- lab-caravel_fir/testbench/counter_la_fir/fir.h
+++ lab-caravel_fir/testbench/counter_la_fir/fir.h
@@ -5,6 +5,6 @@
 
 int taps[N] = {0,-10,-9,23,56,63,56,23,-9,-10,0};
 int inputbuffer[N];
-int inputsignal[N] = {1,2,3,4,5,6,7,8,9,10,11};
+//int inputsignal[N] = {1,2,3,4,5,6,7,8,9,10,11};
 int outputsignal[N];
 #endif
