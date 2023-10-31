83f312e0e80f4583743b93210605daf91e0f2054
ZheChen-Bill <88698677+ZheChen-Bill@users.noreply.github.com>
2023-10-31 10:04:59 -0400

    Add files via upload

--- lab-caravel_fir/rtl/user/bram12.v
+++ lab-caravel_fir/rtl/user/bram12.v
@@ -1,15 +1,13 @@
-module bram11 (clk, we, re, waddr, raddr, wdi, rdo);
-    `define RAMinitFile "./init.dat"
+module bram12 (clk, we, re, waddr, raddr, wdi, rdo);
     parameter ADDR_WIDTH = 12;
     parameter SIZE = 12;
     parameter BIT_WIDTH = 32;
     input                          clk;
-    input                          we, re;
+    input [3:0]                    we, re;
     input [ADDR_WIDTH-1:0]         waddr, raddr;
     input [BIT_WIDTH-1:0]          wdi;
     output reg [BIT_WIDTH-1:0]     rdo;
-    reg [BIT_WIDTH-1:0] RAM [SIZE-1:0];
-    initial $readmemh(`RAMinitFile, RAM);
+    (* ram_style = "block" *) reg [BIT_WIDTH-1:0] RAM [SIZE-1:0];
     
     always @(posedge clk)begin
         if(re) rdo <= RAM[raddr];
