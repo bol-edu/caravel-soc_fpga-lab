470d6a395900b5bd58f5cb8ebfc353a8df40408e
LingChiYang <hisky1256.ee11@nycu.edu.tw>
2023-11-21 20:26:50 +0800

    add uart design

--- lab-wlos_baseline/firmware/isr.c
+++ lab-wlos_baseline/firmware/isr.c
@@ -4,47 +4,52 @@
 #include <csr.h>
 #include <soc.h>
 #include <irq_vex.h>
-#include <uart.h>
+#include <user_uart.h>
 #include <defs.h>
+
+extern int uart_read();
+extern char uart_read_char();
+extern char uart_write_char();
+extern int uart_write();
 
 void isr(void);
 
 #ifdef CONFIG_CPU_HAS_INTERRUPT
 
-uint16_t flag;
+#ifdef USER_PROJ_IRQ0_EN
+uint32_t counter = 0xFFFF0000;
+#endif
 
 void isr(void)
 {
-//	__attribute__((unused)) unsigned int irqs;
-//
-//	irqs = irq_pending() & irq_getmask();
-//
-	irq_setmask(0);
-//
-//    reg_timer0_irq_en = 0; // disable interrupt
-//    reg_debug_irq_en = 0;
 
-//    reg_reset = 1;
-//    asm volatile ("slli x0, x0, 0x1f");
-//    asm volatile ("ebreak");
-//    asm volatile ("srai x0, x0, 7");
+#ifndef USER_PROJ_IRQ0_EN
 
-   reg_la1_data = 0xa;
-   reg_la0_data = 0x20000;
-   flag = 1;
+    irq_setmask(0);
 
-//	if(irqs & (1 << TIMER0_INTERRUPT)) {
-////		uart_isr();
-//        reg_la1_data = 0xa;	// Signal end of test 1st stage
-//        reg_la0_data = 0x20000;
-//        flag = 1;
-//    }
+
+#else
+    uint32_t irqs = irq_pending() & irq_getmask();
+    int buf;
+
+    if ( irqs & (1 << USER_IRQ_0_INTERRUPT)) {
+        user_irq_0_ev_pending_write(1); //Clear Interrupt Pending Event
+        /*if((((reg_uart_stat>>5) | 0) == 0) && (((reg_uart_stat>>4) | 0) == 0)){
+            for(int i = 0; i < 1; i++)
+                asm volatile ("nop");
+
+            reg_mprj_datal = reg_rx_data << 16;
+        }*/
+        //reg_mprj_datal = uart_read() << 16;
+        buf = uart_read();
+        uart_write(buf);
+        reg_mprj_datal = buf << 16;
+
+    }
+#endif
+
     return;
 
-//#ifndef UART_POLLING
-//	if(irqs & (1 << UART_INTERRUPT))
-//		uart_isr();
-//#endif
 }
 
 #else
--- lab-wlos_baseline/vip/tbuart.v
+++ lab-wlos_baseline/vip/tbuart.v
@@ -1,36 +1,201 @@
-`default_nettype none
-/*
- *  SPDX-FileCopyrightText: 2017 Clifford Wolf
- *
- *  PicoSoC - A simple example SoC using PicoRV32
- *
- *  Copyright (C) 2017  Clifford Wolf <clifford@clifford.at>
- *
- *  Permission to use, copy, modify, and/or distribute this software for any
- *  purpose with or without fee is hereby granted, provided that the above
- *  copyright notice and this permission notice appear in all copies.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- *  SPDX-License-Identifier: ISC
- */
-
+`default_nettype wire
 `timescale 1 ns / 1 ps
 
 /* tbuart --- mimic an external UART display, operating at 9600 baud	*/
 /* and accepting ASCII characters for display.				*/
-
-/* To do:  Match a known UART 3.3V 16x2 LCD display.  However, it	*/
-/* should be possible on a testing system to interface to the UART	*/
-/* pins on a Raspberry Pi, also running at 3.3V.			*/
-
 module tbuart (
+	input  ser_rx,
+	input tx_start,
+	output reg ser_tx,
+	input [7:0] tx_data,
+	output reg tx_busy,
+	output reg tx_clear_req
+);
+	reg [2:0] recv_state;
+	reg [2:0] recv_next_state;
+	reg [2:0] rx_index;
+	reg [31:0]clk_div;
+	reg [31:0]clk_cnt;
+	reg [7:0] recv_pattern;
+	reg [8*10-1:0] recv_buf_data;	// 50 characters.  Increase as needed for tests.
+
+	reg [2:0] tr_state;
+	reg [2:0] tr_next_state;
+	reg [2:0] tx_index;
+	reg [7:0] tx_pattern;
+
+	reg clk;
+
+	parameter R_WAIT 		= 3'b000;
+	parameter R_GET_DATA  	= 3'b001;
+	parameter R_STOP_BIT	= 3'b010;
+	parameter T_WAIT		= 3'b000;
+	parameter T_START_BIT   = 3'b001;
+	parameter T_SEND_DATA	= 3'b010;
+	parameter T_STOP_BIT    = 3'b011;
+	parameter T_CLEAR       = 3'b100;
+	
+
+	parameter baud_rate = 9600;
+
+
+	initial begin
+		clk = 0;
+		recv_state = 0;			// 3bit
+		recv_next_state = 0;
+		rx_index = 0;			// 3bit
+		recv_pattern = 0;			// 8bit
+		recv_buf_data = 0;			// 50 words
+		tr_state = 0;
+		tr_next_state = 0;
+		tx_index = 0;
+		tx_pattern = 0;
+		ser_tx = 1;
+		//tx_busy = 0;
+		tx_clear_req = 0;
+		clk_div = 40000000/baud_rate;
+		clk_cnt = 0;
+	end
+
+	always #52083 clk <= ~clk;
+
+	always@(posedge clk)begin
+		recv_state <= recv_next_state;
+	end
+	// --------------------Receiver------------------------//
+	always@(*)begin
+		case(recv_state)
+			R_WAIT:begin
+				if(!ser_rx)
+					recv_next_state = R_GET_DATA;
+				else
+					recv_next_state = R_WAIT;
+			end
+			R_GET_DATA:begin
+				if(rx_index==3'b111)
+					recv_next_state = R_STOP_BIT;
+				else
+					recv_next_state = R_GET_DATA;
+			end
+			R_STOP_BIT:begin
+				if(ser_rx)
+					recv_next_state = R_WAIT;
+				else
+					recv_next_state = R_STOP_BIT;
+			end
+			default:begin
+				recv_next_state = R_WAIT;
+			end
+		endcase  
+	end
+
+	always@(posedge clk)begin
+		case(recv_state)
+			R_WAIT, R_STOP_BIT: rx_index <= 0;
+			R_GET_DATA: rx_index <= rx_index + 1;
+			default: rx_index <= 0;
+		endcase
+	end
+
+	always@(posedge clk)begin
+		case(recv_state)
+			R_WAIT: recv_pattern <= 0;
+			R_GET_DATA:	begin
+				recv_pattern <= {ser_rx, recv_pattern[7:1]};
+				$display("rx data bit index %d: %b", rx_index, ser_rx);
+			end
+			default: recv_pattern <= 0;
+		endcase
+	end
+
+	always@(posedge clk)begin
+		if(recv_state==R_STOP_BIT)begin
+			recv_buf_data <= {recv_buf_data, recv_pattern};
+			$display("recevied word %d", recv_pattern);
+		end
+	end
+	
+	// --------------------Transmitter------------------------//
+	always@(posedge clk)begin
+		tr_state <= tr_next_state;
+	end
+
+	always@(*)begin
+		case(tr_state)
+			T_WAIT:begin
+				if(tx_start)
+					tr_next_state = T_START_BIT;
+				else
+					tr_next_state = T_WAIT;
+			end
+			T_START_BIT:begin
+				tr_next_state = T_SEND_DATA;
+			end
+			T_SEND_DATA:begin
+				if(tx_index==3'b111)
+					tr_next_state = T_STOP_BIT;
+				else
+					tr_next_state = T_SEND_DATA;
+			end
+			T_STOP_BIT:begin
+				tr_next_state = T_CLEAR;
+			end
+			T_CLEAR:begin
+				tr_next_state = T_WAIT;
+			end
+			default:begin
+				tr_next_state = T_WAIT;
+			end
+		endcase
+	end
+
+	always@(posedge clk)begin
+		case(tr_state)
+			T_WAIT, T_START_BIT, T_STOP_BIT, T_CLEAR: tx_index <= 0;
+			T_SEND_DATA: tx_index <= tx_index + 1;
+			default: tx_index <= 0;
+		endcase
+	end
+
+	always@(posedge clk)begin
+		if(tr_state==T_WAIT)
+			tx_pattern <= (tx_start) ? tx_data : 0;
+		else if(tr_state==T_CLEAR)
+			tx_pattern <= 0;
+	end
+
+	always@(posedge clk)begin
+		case(tr_state)
+			T_WAIT: ser_tx <= 1;
+			T_START_BIT: ser_tx <= 0;
+			T_SEND_DATA:begin 
+				ser_tx <= tx_pattern[tx_index];
+				$display("tx data bit index %d: %b", tx_index, tx_pattern[tx_index]);
+			end
+			T_STOP_BIT: ser_tx <= 1;
+			T_CLEAR: ser_tx <= 1;
+			default: ser_tx <= 1;
+		endcase
+	end
+
+	always@(posedge clk)begin
+		if(tr_state==T_CLEAR)
+			tx_clear_req <= 1;
+		else
+			tx_clear_req <= 0;
+	end
+
+	always@(*)begin
+		if(tr_next_state == T_START_BIT)
+			tx_busy = 1;
+		else if(tr_state == T_CLEAR)
+			tx_busy = 0;
+	end
+
+endmodule
+
+
+/*module tbuart (
 	input  ser_rx,
 	output  ser_tx
 );
@@ -55,7 +220,9 @@ module tbuart (
 	// passed specific command words.
 
 //	always #1500 clk <= (clk === 1'b0);
-	always #2650 clk <= (clk === 1'b0);  // working for 9600 baud
+//	always #52083 clk <= (clk === 1'b0);  // working for 9600 baud 52075
+	always #10417 clk <= ~clk;
+//	always #2650 clk <= ~clk;
 //	always #125 clk <= (clk === 1'b0);
 
 	always @(posedge clk) begin
@@ -75,17 +242,19 @@ module tbuart (
 			10: begin
 				if (recv_divcnt > 3'd3) begin
 					// 0x0a = '\n'
-					if (recv_pattern == 8'h0a) begin
-						$display("output: %s", recv_buf_data);
-					end else begin
+					//if (recv_pattern == 8'h0a) begin
+						//$display("output: %h", recv_buf_data);
+					//end else begin
+						$display("output: %h", recv_pattern);
 						recv_buf_data <= {recv_buf_data, recv_pattern};
-					end
+					//end
 					recv_state <= 0;
 				end
 			end
-			default: begin
+			default: begin // DATA BITS 2-9
 				if (recv_divcnt > 3'd3) begin
 					recv_pattern <= {ser_rx, recv_pattern[7:1]};
+					$display("ser_rx index %2d: %b", recv_state-2, ser_rx);
 					recv_state <= recv_state + 1;
 					recv_divcnt <= 0;
 				end
@@ -93,4 +262,4 @@ module tbuart (
 		endcase
 	end
 
-endmodule
+endmodule*/
