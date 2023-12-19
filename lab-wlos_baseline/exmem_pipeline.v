d180f8766ec4a306f54c91458cffc298a71620f1
Bridge of Life Education Lab <98332019+boledulab@users.noreply.github.com>
2023-12-19 21:49:01 +0800

    Update exmem_pipeline.v
    fixed syntax error, and simulate it work.

--- lab-wlos_baseline/exmem_pipeline.v
+++ lab-wlos_baseline/exmem_pipeline.v
@@ -11,81 +11,88 @@
 // dat_o   _________________________________________________d3______d4_d5_______
 // 
 
-`default_nettype none
+`default_nettype wire
+
 module exmem_pipeline #(
-    parameter N     // delay
+    parameter N = 10
 )(
-    input clk,
-    input rst,              // active-high
-    input stb,              // command strobe - request
-    input we,               // 1=write, 0=read
-    input [3:0] sel,        // byte-enable
-    input [31:0]  dat_i,    // data input 
-    input [31:0]  addr,     // address input
-    output ack,             // ack - ready
-    output [31:0] dat_o,    // data output
-);
+    input         clk,
+    input         rst,		// active high (from WB)
+    input         stb,		// command strobe -request
+    input         we,		// 1: write, 0: read
+    input  [3:0]  sel,		// byte-enable
+    input  [31:0] dat_i,	// data in
+    input  [31:0] addr,		// address in
+	
+    output        ack,		// ready
+    output [31:0] dat_o	    	// data out
+    );
 
-// The following are shift_fifo
-// it shifts from N-1 to 0 for every cycles
-reg [N-1:0]  valid_fifo; 
-reg [63:0]   req_fifo[N-1:0];    // request_fifo = {addr, dat_i}
-reg [N-1:0]  we_fifo;            // write enable queue
+    // FIFO for shifting WB request
+    reg   [67:0] req_fifo[N-1:0];
+    reg  [N-1:0] we_fifo;
+    reg  [N-1:0] valid_fifo;
+    reg          ack;
+    
+    // Wishbone INPUT request
+    wire  [67:0] req_in;
+    
+    // BRAM byte enable
+    wire   [3:0] byte_en;
 
-// req_in
-`define  SEL_POS  3:0
-`define  DAT_POS  35:4
-`define  ADR_POS  67:36
-
-wire [63:0] req_in = {addr, dat_i, sel}; 
-
-// initalize valid_fifo, we_fifo and perform shift_fifo
-always @(posedge clk or posedge rst) begin
-    if( rst ) begin
-        valid_fifo <= N'b0;   // initialize
-        we_fifo <= N'b0;
-    end else begin
-        valid_fifo <= {stb, valid_fifo[N-1:1]};
-        we_fifo <= {we, we_fifo[N-1:1]};
+    `define SEL_POS 3:0
+    `define DAT_POS 35:4
+    `define ADR_POS 67:36
+    `define N       10
+    
+    // Initalize FIFO, and perform Shift FIFO
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+	       valid_fifo <= `N'b0;
+	       we_fifo    <= `N'b0;
+        end
+        else begin
+           valid_fifo <= {stb, valid_fifo[N-1:1]};
+           we_fifo    <= {we, we_fifo[N-1:1]};
+        end
     end
-end
-
-// shift req_fifo - it can be implemented with SRAM with read/write pointer
-// but need to take care the 1T delay for read 
-generate
-    genvar j;
-    for (j = 0; j < N-2; j = j + 1) begin : SHIFT_INST_GENERATE
-      always @(posedge clk) begin
-          req_fifo[j] <= req_fifo[j+1];
-      end
+    
+    assign req_in = {addr, dat_i, sel};
+    
+    // Put Wishbone input request into FIFO
+    always @(posedge clk) begin
+        req_fifo[N-1] <= req_in;
     end
-endgenerate
+    
+    // Shift the FIFO
+    integer i;
+    // Unroll to N-2, since the FIFO[N-1] has been assign to input request
+    always @(posedge clk) begin
+        for (i = 0; i < N-2; i = i + 1) begin         
+            req_fifo[i] <= req_fifo[i + 1];
+        end
+    end
+    
+    // ACK signal is generated 1T after valid_fifo[0] = 1,
+    // because BRAM read access takes 1T module bram
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            ack <= 0;
+        end
+        else begin
+            ack <= valid_fifo[0];
+        end
+    end
 
-always @(posedge clk) begin 
-    req_fifo[N-1] <= {addr, dat_i, sel};
-
-//
-// ack is generate 1T after valid_fifo[0] = 1 because BRAM read access take 1T delay
-// 
-reg ack;
-always @(posedge clk or posedge rst) begin
-    if( rst ) begin
-        ack <= 0;
-    end else
-        ack <= valid_fifo[0];
-end
-
-
-// access bram - block ram 
-wire [3:0] byte_we = req_fifo[0][`SEL_POS] & 4{we_fifo[0]};
-
+    assign byte_en = req_fifo[0][`SEL_POS] & {4{we_fifo[0]}};
+    
     bram user_bram (
-        .CLK(clk),
-        .WE0(byte_we),
-        .EN0(valid_fifo[0]),
-        .Di0(req_fifo[0][`DAT_POS]),
-        .Do0(dat_o),
-        .A0(req_fifo[0][`ADR_POS])
+        .CLK (clk),
+        .WE0 (byte_en),
+        .EN0 (valid_fifo[0]),
+        .Di0 (req_fifo[0][`DAT_POS]),
+        .Do0 (dat_o),
+        .A0  (req_fifo[0][`ADR_POS])
     );
 
 endmodule
